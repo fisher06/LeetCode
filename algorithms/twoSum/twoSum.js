0b509ace719c3fa3ce4c7fe67b0bd44e5eae3cda
Fisher <wei.xie8906@gmail.com>
2020-11-03 17:15:13 +0100

    Update twoSum.js

--- algorithms/twoSum/twoSum.js
+++ algorithms/twoSum/twoSum.js
@@ -25,3 +25,31 @@ var twoSum = function(nums, target) {
 
     return {}; 
 };
+
+
+/********************************************************************************** 
+Solution 2
+Runtime: 72 ms, faster than 96.32% of JavaScript online submissions for Two Sum.
+Memory Usage: 41.1 MB, less than 5.69% of JavaScript online submissions for Two Sum.
+**********************************************************************************/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var dict = {}
+    for(var i = 0; i < nums.length; i++) {
+        var cur = nums[i];
+        var val = target-cur;
+        if(val in dict) {
+            return [dict[val], i];
+        }
+        
+        dict[cur] = i;
+    }
+
+    return null;
+    
+};
