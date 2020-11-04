363c27619f75c16dceeb753ecc78b898529d3894
Fisher <wei.xie8906@gmail.com>
2020-11-04 15:46:48 +0100

    Update palindromeNumber.js

--- algorithms/palindromeNumber/palindromeNumber.js
+++ algorithms/palindromeNumber/palindromeNumber.js
@@ -1,1 +1,9 @@
+// Source : https://leetcode.com/problems/palindrome-number/
+// Author : Wei XIE
+// Date   : 2020-11-03
 
+/********************************************************************************** 
+Solution 1
+Runtime: 100 ms, faster than 50.95% of JavaScript online submissions for Reverse Integer.
+Memory Usage: 40.1 MB, less than 18.65% of JavaScript online submissions for Reverse Integer.
+**********************************************************************************/
