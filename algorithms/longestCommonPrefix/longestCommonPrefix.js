bfd0d59644887ad811328752f2f3f9c551ea603b
Fisher <wei.xie8906@gmail.com>
2020-11-12 17:43:22 +0100

    Update longestCommonPrefix.js

--- algorithms/longestCommonPrefix/longestCommonPrefix.js
+++ algorithms/longestCommonPrefix/longestCommonPrefix.js
@@ -4,6 +4,34 @@
 
 /********************************************************************************** 
 Solution 1
-Runtime: 216 ms, faster than 69.52% of JavaScript online submissions for Palindrome Number.
-Memory Usage: 48.1 MB, less than 5.85% of JavaScript online submissions for Palindrome Number.
+Runtime: 96 ms, faster than 21.48% of JavaScript online submissions for Longest Common Prefix.
+Memory Usage: 39.7 MB, less than 5.53% of JavaScript online submissions for Longest Common Prefix.
 **********************************************************************************/
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    
+    if (!strs.length) {
+        return "";
+    }
+    
+    let prefix = "";
+    
+    for (let i = 0; i < strs[0].length; i++) {
+        
+        for (let j = 0; j < strs.length-1; j++) {
+            
+            if (strs[j][i] !== strs[j+1][i]) {
+                
+                return prefix;
+                
+            }
+        }
+        
+        prefix += strs[0][i];
+    }
+    
+    return prefix;
+};
