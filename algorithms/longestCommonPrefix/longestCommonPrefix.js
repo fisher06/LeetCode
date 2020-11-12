e6a22dea8b2fa5cfe38ca3ea869726530e1d353a
Fisher <wei.xie8906@gmail.com>
2020-11-12 17:11:52 +0100

    Create longestCommonPrefix.js

---
+++ algorithms/longestCommonPrefix/longestCommonPrefix.js
@@ -0,0 +1,9 @@
+// Source : https://leetcode.com/problems/longest-common-prefix/
+// Author : Wei XIE
+// Date   : 2020-11-03
+
+/********************************************************************************** 
+Solution 1
+Runtime: 216 ms, faster than 69.52% of JavaScript online submissions for Palindrome Number.
+Memory Usage: 48.1 MB, less than 5.85% of JavaScript online submissions for Palindrome Number.
+**********************************************************************************/
