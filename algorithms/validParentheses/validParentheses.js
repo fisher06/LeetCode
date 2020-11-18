b3dd8c97a3e2805dfc28f4963b3cc61c151ff967
Fisher <wei.xie8906@gmail.com>
2020-11-18 16:08:51 +0100

    Update validParentheses.js

--- algorithms/validParentheses/validParentheses.js
+++ algorithms/validParentheses/validParentheses.js
@@ -8,3 +8,32 @@ Solution 1
 Runtime: 96 ms, faster than 21.48% of JavaScript online submissions for Longest Common Prefix.
 Memory Usage: 39.7 MB, less than 5.53% of JavaScript online submissions for Longest Common Prefix.
 **********************************************************************************/
+/**
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+    let test = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        
+        let char = s[i];
+        
+        if (char === '(') {
+            test.push(')');
+        } else if (char === '[') {
+            test.push(']');
+        }  else if (char === '{') {
+            test.push('}');
+        } else {
+            if (test.pop() !== char) {
+                return false;
+            }
+        }     
+    }
+    
+    return test.length ? false : true;
+    
+};
