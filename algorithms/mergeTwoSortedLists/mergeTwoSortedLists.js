4993c00561619c4c377f2dde8347f799144ab611
Fisher <wei.xie8906@gmail.com>
2020-11-18 16:24:20 +0100

    Create mergeTwoSortedLists.js

---
+++ algorithms/mergeTwoSortedLists/mergeTwoSortedLists.js
@@ -0,0 +1,9 @@
+// Source : https://leetcode.com/problems/merge-two-sorted-lists/
+// Author : Wei XIE
+// Date   : 2020-11-18
+
+/********************************************************************************** 
+Solution 1
+Runtime: 80 ms, faster than 92.53% of JavaScript online submissions for Valid Parentheses.
+Memory Usage: 33.9 MB, less than 99.98% of JavaScript online submissions for Valid Parentheses.
+**********************************************************************************/
