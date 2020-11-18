44a075a8bc10a4b57e285420657d1b035eea3e1e
Fisher <wei.xie8906@gmail.com>
2020-11-18 15:10:06 +0100

    Create validParentheses.js

---
+++ algorithms/validParentheses/validParentheses.js
@@ -0,0 +1,10 @@
+
+// Source : https://leetcode.com/problems/valid-parentheses/
+// Author : Wei XIE
+// Date   : 2020-11-18
+
+/********************************************************************************** 
+Solution 1
+Runtime: 96 ms, faster than 21.48% of JavaScript online submissions for Longest Common Prefix.
+Memory Usage: 39.7 MB, less than 5.53% of JavaScript online submissions for Longest Common Prefix.
+**********************************************************************************/
