48c5ea56d5e84ba1b3d8571fa11ac93d5bd9838c
Fisher <wei.xie8906@gmail.com>
2020-11-19 16:48:15 +0100

    Create removeDuplicatesFromSortedArray.js

---
+++ algorithms/removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.js
@@ -0,0 +1,10 @@
+
+// Source : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// Author : Wei XIE
+// Date   : 2020-11-19
+
+/********************************************************************************** 
+Solution 1
+Runtime: 96 ms, faster than 21.48% of JavaScript online submissions for Longest Common Prefix.
+Memory Usage: 39.7 MB, less than 5.53% of JavaScript online submissions for Longest Common Prefix.
+**********************************************************************************/
