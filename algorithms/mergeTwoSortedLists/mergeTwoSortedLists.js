1b28591b6c116f5640fc5f56ef00e31f5e8b898d
Fisher <wei.xie8906@gmail.com>
2020-11-20 19:07:00 +0100

    Update mergeTwoSortedLists.js

--- algorithms/mergeTwoSortedLists/mergeTwoSortedLists.js
+++ algorithms/mergeTwoSortedLists/mergeTwoSortedLists.js
@@ -4,6 +4,38 @@
 
 /********************************************************************************** 
 Solution 1
-Runtime: 80 ms, faster than 92.53% of JavaScript online submissions for Valid Parentheses.
-Memory Usage: 33.9 MB, less than 99.98% of JavaScript online submissions for Valid Parentheses.
+Runtime: 88 ms, faster than 80.04% of JavaScript online submissions for Merge Two Sorted Lists.
+Memory Usage: 40 MB, less than 82.79% of JavaScript online submissions for Merge Two Sorted Lists.
 **********************************************************************************/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    let dummy = new ListNode(0);
+    let n = dummy;
+    
+    let n1 = l1, n2 = l2;
+    
+    while (n1 || n2) {
+        if (!n2 || (n1 && n1.val < n2.val)) {
+            n.next = new ListNode(n1.val);
+            n1 = n1.next;
+        } else {
+            n.next = new ListNode(n2.val);
+            n2 = n2.next;
+        }
+        
+        n = n.next;
+    }
+    
+    return dummy.next;
+};
