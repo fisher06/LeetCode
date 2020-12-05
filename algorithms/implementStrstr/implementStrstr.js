38f2243ea14b1a4bad418ba6efcd31265bce7710
Fisher <wei.xie8906@gmail.com>
2020-12-05 21:39:15 +0100

    Create implementStrstr.js

---
+++ algorithms/implementStrstr/implementStrstr.js
@@ -0,0 +1,9 @@
+// Source : https://leetcode.com/problems/implement-strstr/
+// Author : Wei XIE
+// Date   : 2020-11-03
+
+/********************************************************************************** 
+Solution 1
+Runtime: 96 ms, faster than 21.48% of JavaScript online submissions for Longest Common Prefix.
+Memory Usage: 39.7 MB, less than 5.53% of JavaScript online submissions for Longest Common Prefix.
+**********************************************************************************/
