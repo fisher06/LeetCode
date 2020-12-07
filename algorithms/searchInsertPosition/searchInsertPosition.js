2c1e98626cd52a7d680d802dce915b69af0aade0
Fisher <wei.xie8906@gmail.com>
2020-12-07 16:25:39 +0100

    Update searchInsertPosition.js

--- algorithms/searchInsertPosition/searchInsertPosition.js
+++ algorithms/searchInsertPosition/searchInsertPosition.js
@@ -4,5 +4,43 @@
 
 /********************************************************************************** 
 Solution 1
-
+Runtime: 84 ms, faster than 20.14% of JavaScript online submissions for Search Insert Position.
+Memory Usage: 38.8 MB, less than 43.14% of JavaScript online submissions for Search Insert Position.
 **********************************************************************************/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    
+    if (target <= nums[0]) { 
+        return 0 
+    }
+    
+    if (target > nums[nums.length -1]) { 
+        return  nums.length
+    }
+    
+    for (var i = 0; i < nums.length; i++) {
+        if (target <= nums[i]) {
+            return i
+        }
+    }
+};
+
+/********************************************************************************** 
+Solution 2
+Runtime: 72 ms, faster than 93.41% of JavaScript online submissions for Search Insert Position.
+Memory Usage: 38.7 MB, less than 43.14% of JavaScript online submissions for Search Insert Position.
+**********************************************************************************/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let i = 0;
+    while(nums[i] < target) i += 1;
+    return i;    
+};
