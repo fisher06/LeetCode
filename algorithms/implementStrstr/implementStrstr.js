02206858930b32427a0891689d80b4b959866256
Fisher <wei.xie8906@gmail.com>
2020-12-07 16:10:53 +0100

    Update implementStrstr.js

--- algorithms/implementStrstr/implementStrstr.js
+++ algorithms/implementStrstr/implementStrstr.js
@@ -19,3 +19,45 @@ var strStr = function(haystack, needle) {
         return 0
     }
 };
+
+/********************************************************************************** 
+Solution 2
+Runtime: 80 ms, faster than 68.47% of JavaScript online submissions for Implement strStr().
+Memory Usage: 39 MB, less than 52.58% of JavaScript online submissions for Implement strStr().
+**********************************************************************************/
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+    if(!needle) {
+        return 0;
+    }
+    
+    if(!haystack) {
+        return -1;
+    }
+    
+    if(haystack.length<needle.length) {
+        return -1;
+    } 
+    
+    let i=0;
+    let j=0;
+    
+    while(i<haystack.length && j<needle.length){
+        
+        if (haystack[i]===needle[j]){
+            i++;
+            j++;
+        } else {
+            i = (++i) - j;
+            j = 0;
+        }
+    }
+    if(j===needle.length) return i-j;
+
+    return -1;
+};
