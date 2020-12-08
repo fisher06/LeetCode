89150cd169f35e6c243445678f170adb361a84d8
Fisher <wei.xie8906@gmail.com>
2020-12-08 17:13:19 +0100

    Update countAndSay.js

--- algorithms/countAndSay/countAndSay.js
+++ algorithms/countAndSay/countAndSay.js
@@ -4,5 +4,39 @@
 
 /********************************************************************************** 
 Solution 1
-
+Runtime: 80 ms, faster than 80.05% of JavaScript online submissions for Count and Say.
+Memory Usage: 41 MB, less than 42.53% of JavaScript online submissions for Count and Say.
 **********************************************************************************/
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    if (n === 1) {
+        
+        return '1'
+    } else {
+        
+        let result = '1';
+
+        for (let i = 2; i <= n; i++) {
+            
+            let num = '', current = result[0], count = 0, j = 0;
+        
+            while (j < result.length) {
+                
+                while (j < result.length && current === result[j]) { 
+                    count++; j++; 
+                }
+
+                num += count + current;
+                current = result[j]; 
+                count = 0;
+            }
+        
+            result = num        
+        }
+    
+        return result;
+   }
+};
