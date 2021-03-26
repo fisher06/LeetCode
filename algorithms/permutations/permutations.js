5de074983ca22f49bc010867e0af9624614e8fb6
Fisher <wei.xie8906@gmail.com>
2021-03-26 22:05:55 +0100

    Update permutations.js

--- algorithms/permutations/permutations.js
+++ algorithms/permutations/permutations.js
@@ -40,3 +40,42 @@ var permute = function(nums) {
     return res;
     
 };
+
+/********************************************************************************** 
+Solution 2
+Runtime: 96 ms, faster than 69.48% of JavaScript online submissions for Permutations.
+Memory Usage: 41.8 MB, less than 53.91% of JavaScript online submissions for Permutations.
+**********************************************************************************/
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+var permute = function(nums) {
+
+    let res = [];
+    let track = [];
+    
+    let backtrack = (nums, track) => {
+
+        if (track.length === nums.length){
+            res.push([...track]);
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            
+            if (track.includes(nums[i])) continue;
+            
+            track.push(nums[i]);                
+            backtrack(nums, track);
+            track.pop();
+            
+        }
+        
+    }
+    
+    backtrack(nums, track);
+    return res;
+    
+};
