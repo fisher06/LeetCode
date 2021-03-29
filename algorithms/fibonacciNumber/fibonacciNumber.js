d41640417948e5468b4cc520b27feb5c6fb01ccf
Fisher <wei.xie8906@gmail.com>
2021-03-29 21:02:06 +0200

    Update fibonacciNumber.js

--- algorithms/fibonacciNumber/fibonacciNumber.js
+++ algorithms/fibonacciNumber/fibonacciNumber.js
@@ -4,5 +4,25 @@
 
 /********************************************************************************** 
 Solution 1
-
+Runtime: 80 ms, faster than 60.81% of JavaScript online submissions for Fibonacci Number.
+Memory Usage: 38.1 MB, less than 92.11% of JavaScript online submissions for Fibonacci Number.
 **********************************************************************************/
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    
+    if (n <= 1) return n;
+    
+    let curr = 1;
+    let prev = 0;
+    
+    for (let i = 2; i <= n; i++){
+        let sum = curr + prev;
+        prev = curr;
+        curr = sum;
+    }
+    
+    return curr;
+};
