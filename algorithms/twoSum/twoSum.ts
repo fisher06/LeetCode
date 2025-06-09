94db726a140979d2796c8ba75a844c5794df01e3
Fisher <wei.xie8906@gmail.com>
2025-06-09 21:28:53 +0200

    Update twoSum.ts

--- algorithms/twoSum/twoSum.ts
+++ algorithms/twoSum/twoSum.ts
@@ -1,1 +1,18 @@
+// Source : https://leetcode.com/problems/two-sum/
+// Author : Wei XIE
+// Date   : 2025-06-09
 
+
+function twoSum(nums: number[], target: number): number[] {
+  let map = new Map<number, number>();
+
+  for(let i = 0; i < nums.length; i++ ) {
+    let complement = target - nums[i];
+
+    if (map.has(complement)) {
+        return [i, map.get(complement)]
+    }
+    map.set(nums[i], i);
+  } 
+
+};
