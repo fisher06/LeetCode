b22d1be974d5b6f0af0f342eedb58f5fed89b9fb
Fisher <wei.xie8906@gmail.com>
2025-06-09 21:55:40 +0200

    Update longestSubstringWithoutRepeatingCharacters.ts

--- algorithms/longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.ts
+++ algorithms/longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.ts
@@ -1,1 +1,22 @@
+// Source : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// Author : Wei XIE
+// Date   : 2025-06-09
 
+function lengthOfLongestSubstring(s: string): number {
+    const seen = new Set<String>();
+
+    let left = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (seen.has(s[right])) {
+            seen.delete(s[left]);
+            left++
+        }
+
+        seen.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength
+};
